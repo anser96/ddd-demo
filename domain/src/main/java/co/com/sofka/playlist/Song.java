dbc496ee738e7e6120bfb0c2ca35c7e174630069
OscarSofka <107581543+OscarSofka@users.noreply.github.com>
2022-06-16 12:43:18 -0500

    Update Song.java
    cambios en song

--- domain/src/main/java/co/com/sofka/playlist/Song.java
+++ domain/src/main/java/co/com/sofka/playlist/Song.java
@@ -1,34 +1,40 @@
 package co.com.sofka.playlist;
 
 import co.com.sofka.domain.generic.Entity;
-import co.com.sofka.playlist.values.*;
+import co.com.sofka.playlist.values.SongArtist;
+import co.com.sofka.playlist.values.SongDate;
+import co.com.sofka.playlist.values.SongName;
 
-public class Song extends Entity<SongId> {
+public class Song extends Entity<SongName> {
     protected SongArtist songArtist;
-
     protected SongDate songDate;
+    protected SongDuration songDuration;
+    protected SongAlbum songAlbum;
+    protected SongGenre songGenre;
+    protected SongComposer songComposer;
 
-    protected SongGender gender;
+    public Song(SongName songName, SongArtist songArtist, SongDate songDate, SongDuration songDuration,
+                SongAlbum songAlbum, SongGenre songGenre, SongComposer songComposer){
 
-
-    public Song(SongId songId,SongName songName, SongArtist songArtist, SongDate songDate, SongGender gender){
-        super(songId);
+        super(songName);
         this.songArtist = songArtist;
         this.songDate = songDate;
-        this.gender = gender;
+        this.songDuration = songDuration;
+        this.songAlbum = songAlbum;
+        this.songGenre = songGenre;
+        this.songComposer = songComposer;
     }
-
 
     public void ChangeArtist(SongArtist songArtist){
         this.songArtist = songArtist;
     }
-
     public SongArtist songArtist(){
         return songArtist;
     }
-
     public SongDate songDate(){
         return songDate;
     }
-
-}
+    public SongDuration SongDuration() { return songDuration; }
+    public SongAlbum SongAlbum() { return songAlbum; }
+    public SongGenre getSongGenre() { return songGenre; }
+    public SongComposer getSongComposer() { return songComposer; }
